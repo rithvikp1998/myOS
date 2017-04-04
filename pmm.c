41af19fd521686e9894e27b6869f3efd07b58071
Rithvik Patibandla <rithvikp98@gmail.com>
2017-04-04 20:00:53 +0530

    Added some pmm functions

--- pmm.c
+++ pmm.c
@@ -28,11 +28,26 @@ void get_mbmmap(void){
 	memory_map_t* mmap = (memory_map_t*)(mbinfo_addr -> mmap_addr + 0xC0000000);
 	for(uint32_t i=mmap->base_addr_low; i<mmap->base_addr_low+mmap->length_low; i+=4096)
 		if(mmap->type==1 && i>640*1024)
-			mem_bitmap[i/(4096*64)] &= 0 << ((i/4096) % 64);
+			mem_bitmap[i/(4096*64)] &= ~(1 << ((i/4096) % 64));
 	while((uint32_t)mmap < mbinfo_addr->mmap_addr + 0xC0000000 + mbinfo_addr->mmap_length){
 		mmap = (memory_map_t*)((uint32_t)mmap + mmap->size + sizeof(mmap->size));
 		for(uint32_t i=mmap->base_addr_low; i<mmap->base_addr_low+mmap->length_low; i+=4096)
 			if(mmap->type==1 && i>640*1024)
-				mem_bitmap[i/(4096*64)] &= 0 << ((i/4096) % 64);
+				mem_bitmap[i/(4096*64)] &= ~(1 << ((i/4096) % 64));
 	}
+}
+
+uint32_t get_free_block(void){
+	for(uint16_t i=2;i<16*1024;i++)
+		if(mem_bitmap[i]!=0xFFFFFFFFFFFFFFFF)
+			for(uint8_t j=0;j<64;j++)
+				if(!(mem_bitmap[i]&(1<<j))){
+					mem_bitmap[i]|=(1<<j);
+					return (i*64+j)*4096;
+				}
+	return -1;
+}
+
+void free_block(uint32_t address){
+	mem_bitmap[address/(4096*64)] &= ~(1 << ((address/4096) % 64));
 }
--- pmm.h
+++ pmm.h
@@ -5,6 +5,9 @@
 
 multiboot_info_t* mbinfo_addr;
 uint64_t mem_bitmap[1024*16];	/* Each uint64_t can address 64 x 4K = 256K */
-void get_mbmmap(void);			/* For a max of 4G physical memory, we need */
-								/* 4G/256K = 1024 x 16 long long ints */
+								/* For a max of 4G physical memory, we need */
+void get_mbmmap(void);			/* 4G/256K = 1024 x 16 long long ints */
+uint32_t get_free_block(void);	
+void free_block(uint32_t address);
+
 #endif
