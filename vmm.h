c8a331b7e98bb00c8752bf8b6e8c1c4bd8ce25fa
Rithvik Patibandla <rithvikp98@gmail.com>
2017-04-15 11:59:32 +0530

    Renamed paging files as vmm

---
+++ vmm.h
@@ -0,0 +1,13 @@
+#ifndef VMM_H
+#define VMM_H
+
+extern void load_page_directory(uint32_t* page_directory);
+extern void enable_paging(void);
+
+uint32_t page_directory[1024]__attribute__((aligned(4096)));		
+uint32_t first_page_table[1024]__attribute__((aligned(4096)));		
+
+void fill_page_directory(void);
+void fill_first_page_table(void);
+
+#endif
